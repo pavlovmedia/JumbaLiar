4953939b34a5d5bb4a6eb36ab4b91ce2deaa602b
James Braun <jbraun@mac-dev-053.lan>
2023-05-31 12:00:17 -0500

    Porting Vue application to Nuxt

--- JumbaLiar/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from 'primevue/config';
-
-const app = createApp(App)
-app.mount('#app')
-app.use(PrimeVue);
