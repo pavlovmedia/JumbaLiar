241d4f99247bfeae089d4c9b26d8553df1a8d9d8
James Braun <jbraun@mac-dev-053.local>
2023-08-22 09:51:08 -0500

    DB connection doesn't throw errors anymore

--- JumbaLiar/server/api/endpoint.js
+++ JumbaLiar/server/api/endpoint.js
@@ -1,16 +1,16 @@
-import { db } from "";
+import db from "~/db";
 import jwt from "jsonwebtoken";
 
 export const post = async request => {
   if (!request.headers.authorization) { return { status: 403 } }
-    const token = jwt.verify(request.headers.authorization, import.meta.env.VITE_PWD_SALT);
-    if (!token) { return { status: 403 } }
-    // Perform the action
-    let user = await db('users').where('email', request.body.email);
-    if (user.length === 0) { return { error: '404' } }
-    let guest = await db('guests').insert({ userId: user[0].id, listId: request.body.listId });
-    // Return
-    return {
-      body: guest
-    }
+  const token = jwt.verify(request.headers.authorization, import.meta.env.VITE_PWD_SALT);
+  if (!token) { return { status: 403 } }
+  // Perform the action
+  let user = await db('users').where('email', request.body.email);
+  if (user.length === 0) { return { error: '404' } }
+  let guest = await db('guests').insert({ userId: user[0].id, listId: request.body.listId });
+  // Return
+  return {
+    body: guest
+  }
 }
